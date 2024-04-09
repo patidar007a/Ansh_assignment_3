ac3d167f5c939cf0c351b96fadb0d8432ed577ec
patidar007a <anshupatidar.com>
2024-04-09 14:53:10 -0400

    structure changed

--- app.py
+++ app.py
@@ -1,7 +1,44 @@
+"""
+Employee Management System Module
+
+This module provides various functionalities for managing employee data, 
+including adding, deleting, updating, and generating reports.
+"""
 import src.menu as menu
+
 def main():
+    """
+    Main function to start the Employee Management System.
+    
+    This function calls the main_menu() function to display the main menu
+    of the Employee Management System to the user.
+    """
     menu.main_menu()
+
 if __name__ == "__main__":
     main()
+    
+def add_employee(employees):
+    name = input("Enter employee name: ")
+    age = input("Enter employee age: ")
+    position = input("Enter employee position: ")
 
+    new_employee = {
+        'name': name,
+        'age': age,
+        'position': position
+    }
 
+    employees.append(new_employee)
+    print("Employee added successfully.")
+
+# Example usage:
+# Initialize an empty list to store employees
+employees_list = []
+
+# Call the function to add an employee
+add_employee(employees_list)
+
+# Print the updated list of employees
+print("Updated List of Employees:")
+print(employees_list)
--- import requests.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-api_key = 'b3dc1e50a6bd71afbbd99555bb2034ba'
-city = input('Enter the name of city : ')
