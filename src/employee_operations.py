4ad11485d4cdcba8c6c21cb7bec68fe4b714c147
patidar007a <anshupatidar.com>
2024-04-11 16:40:50 -0400

    coded for delete employee added

--- src/employee_operations.py
+++ src/employee_operations.py
@@ -14,21 +14,47 @@ def add_employee(first_name, last_name, dob, department, salary):
     to the system.
 
     """
+class Employee:
+    def __init__(self, id, first_name, last_name, dob, department, salary):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.dob = dob
+        self.department = department
+        self.salary = salary
 
-    list_emp = read_employees()
-    last_emp = list_emp[len(list_emp)-1]
-    last_id =last_emp['ID']
-    new_employee = {
-        'ID': last_id,
-        'first_name': first_name,
-        'last_name': last_name,
-        'dob': dob,
-        'department': department,
-        'salary': salary
-    }
-    list_emp.append(new_employee)
-    write_employees(list_emp)
+def delete_employee(employees):
+    employee_id = input("Enter the ID which you want to delete: ")
+    found = False
+    for employee in employees:
+        if employee.id == int(employee_id): 
+            employees.remove(employee)
+            found = True
+            print("You successfully delete the employee.")
+            break
+    if not found:
+        print("Given employee ID is not found.")
 
+
+    with open("employee_data.txt", "w") as file:
+        for employee in employees:
+            file.write(f"{employee.id},{employee.first_name},{employee.last_name},{employee.dob},{employee.department},{employee.salary}\n")
+
+#nnnnnnnnnnnnnnnnnnnnnnnnnn
+if __name__ == "__main__":
+    employees = [] 
+
+    while True:
+        print("\n1. Delete Employee")
+        print("2. Exit")
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            delete_employee(employees)
+        elif choice == "2":
+            break
+        else:
+            print("Invalid choice. Please enter a valid option.")
 
 def delete_employee(id):
     """
@@ -38,6 +64,17 @@ def delete_employee(id):
     removes the employee from the system.
 
     """
+    employee_id = input("Enter the ID which you want to delete: ")
+    found = False
+    for employee in employees:
+        if employee.id == int(employee_id): 
+            employees.remove(employee)
+            found = True
+            print("You successfully delete the employee.")
+            break
+    if not found:
+        print("Given employee ID is not found.")
+
 
 def update_employee():
     """
